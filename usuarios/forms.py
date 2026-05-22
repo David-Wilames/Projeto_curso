0589264ac2e2c8b6fe7a87c962e1a743d81b2795
David_Wilames <davidwilames2005@gmail.com>
2026-05-21 22:17:44 -0300

    Atualização

---
+++ usuarios/forms.py
@@ -0,0 +1,94 @@
+from django import forms
+
+class LoginForm(forms.Form):
+    nome_login = forms.CharField(
+        label='Nome de Login',
+        required=True,
+        max_length=100,
+        widget=forms.TextInput(
+          attrs={ 
+            "class": "form-control",
+            "placeholder": "Digite seu nome de login",
+            }
+        )
+    )
+    senha = forms.CharField(
+        label='Senha',
+        required=True,
+        max_length=70,
+        widget=forms.PasswordInput(
+        attrs={ 
+            "class": "form-control",
+            "placeholder": "Digite sua senha"
+            }
+        )
+    )
+
+class CadastroForm(forms.Form):
+    nome_cadastro = forms.CharField(
+        label='Nome de Cadastro',
+        required=True,
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Digite seu nome de cadastro"
+            }
+        )
+    )
+    email = forms.EmailField(
+        label='Email de Cadastro',
+        required=True,
+        widget=forms.EmailInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Digite seu email"
+            }
+        )
+    )
+    senha_1 = forms.CharField(
+        label='Senha',
+        required=True,
+        max_length=70,
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Digite sua senha"
+            }
+        )
+    )
+    senha_2 = forms.CharField(
+        label='Confirmação de Senha',
+        required=True,
+        max_length=70,
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Confirme sua senha"
+            }
+        )
+    )
+
+    def clean_nome_cadastro(self):
+        nome = self.cleaned_data.get('nome_cadastro')
+
+        if nome:
+            nome = nome.strip()
+            if ' ' in nome:
+                raise forms.ValidationError('O nome de cadastro não pode conter espaços.')
+            else:
+                return nome
+            
+    def clean_senha_2(self):
+        senha_1 = self.cleaned_data.get('senha_1')
+        senha_2 = self.cleaned_data.get('senha_2')
+
+        if senha_1 and senha_2:
+            if senha_1 != senha_2:
+                raise forms.ValidationError('As senhas não são iguais.')
+            else:
+                return senha_2
+        
+       
+
+
